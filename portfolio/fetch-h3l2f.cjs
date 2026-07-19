d86db38d682ec37382c91c1ff53a37027e1e1f87
trungtinhxd <switchmitch715@gmail.com>
2026-07-19 13:12:03 +0000

    Deploy source code

---
+++ portfolio/fetch-h3l2f.cjs
@@ -0,0 +1,13 @@
+const https = require('https');
+
+https.get('https://h3l2f.online/', (res) => {
+  let data = '';
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+  res.on('end', () => {
+    console.log(data);
+  });
+}).on('error', (err) => {
+  console.log('Error: ' + err.message);
+});
